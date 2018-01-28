5c2a11e6fd30f9b9f21e7d8b31f8994868da6bc2
marcosradix <marcosradix@gmail.com>
2018-01-28 12:33:18 -0300

    definido camppo id com readonly com jquery

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function($){
+	
 	$('.myid').keyup(function() {
 		  $(this).val(this.value.replace(/\D/g, ''));
 		});
 	$('.cpf').keyup(function() {
 		  $(this).val(this.value.replace(/\D/g, ''));
 		});
+	$('.myid').prop('readonly', true);
 });
