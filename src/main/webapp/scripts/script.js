9fe39de64e52ec52ccca979d416aef86f6442519
marcosradix <marcosradix@gmail.com>
2018-01-28 11:27:51 -0300

    usando scripts fora da página

---
+++ src/main/webapp/scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function($){
+	$('.myid').keyup(function() {
+		  $(this).val(this.value.replace(/\D/g, ''));
+		});
+	$('.cpf').keyup(function() {
+		  $(this).val(this.value.replace(/\D/g, ''));
+		});
+});
